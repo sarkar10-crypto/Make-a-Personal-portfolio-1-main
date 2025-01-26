00834b3857efd9580029ce31867a12d8107514d5
sarkar10-crypto <dayalkumarsarkar11@gmail.com>
2025-01-26 18:56:14 +0530

    adding logo and images, updating some part

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["frontend-developer", "java programmer","web Developer"],
+    strings:["frontend-developer","web Developer","Logo Designer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
